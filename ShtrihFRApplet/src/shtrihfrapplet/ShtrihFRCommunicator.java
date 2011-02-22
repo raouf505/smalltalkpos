cafd2e5eb753b652f394f8ecf093ce80948cf9d0
Assargaddon <Assargaddon@gmail.com>
2011-02-22 17:07:13 +0000

    removed unneccessary comment

--- ShtrihFRApplet/src/shtrihfrapplet/ShtrihFRCommunicator.java
+++ ShtrihFRApplet/src/shtrihfrapplet/ShtrihFRCommunicator.java
@@ -258,18 +258,3 @@ public class ShtrihFRCommunicator {
     }
 
 }
-
-
-/*try
-{
-    // Using Thread.sleep() we can add delay in our application in
-    // a millisecond time. For the example below the program will
-    // take a deep breath for one second before continue to print
-    // the next value of the loop.
-    Thread.sleep(1000);
-    // The Thread.sleep() need to be executed inside a try-catch
-    // block and we need to catch the InterruptedException.
-} catch (InterruptedException ie)
-{
-    ie.printStackTrace();
-}*/

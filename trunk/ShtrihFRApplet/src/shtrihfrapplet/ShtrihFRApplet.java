a7e9e1eff6f0812c25aad418098ef5992d3b360a
Assargaddon@gmail.com <Assargaddon@gmail.com@e8af3c8d-9f29-9bc3-54ef-30cd0e15ad69>
2011-02-22 19:57:42 +0000

    Different "fancy" stuff added: bluring of afocus, if gain one, status (instead of tooltip), maybe something also

--- trunk/ShtrihFRApplet/src/shtrihfrapplet/ShtrihFRApplet.java
+++ trunk/ShtrihFRApplet/src/shtrihfrapplet/ShtrihFRApplet.java
@@ -6,17 +6,17 @@ package shtrihfrapplet;
 
 import java.awt.*;
 import java.applet.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import netscape.javascript.JSObject;
 
 
-public class ShtrihFRApplet extends Applet {
+public class ShtrihFRApplet extends Applet
+    implements FocusListener, MouseListener{
     int[] message, answer;
     String callback;
     ShtrihFRCommunicator communicator = new ShtrihFRCommunicator();
@@ -25,10 +25,14 @@ public class ShtrihFRApplet extends Applet {
     private Image waitImage;
     private Image errorImage;
     private Image okImage;
+    private String appletName;
 
     @Override
     public void init(){
         String tmp;
+
+        tmp=getParameter("appletName");
+        if (tmp!=null) appletName = tmp; else appletName="ShtrihFRApplet";
 
         tmp=getParameter("portName");
         if (tmp!=null) communicator.portName = tmp; else communicator.portName="COM1";
@@ -48,6 +52,9 @@ public class ShtrihFRApplet extends Applet {
         waitImage = getImage(ShtrihFRApplet.class.getResource("res/yellow-led.png"));
         errorImage = getImage(ShtrihFRApplet.class.getResource("res/red-led.png"));
         okImage = getImage(ShtrihFRApplet.class.getResource("res/green-led.png"));
+
+        addFocusListener(this);
+        addMouseListener(this);
     }
 
   
@@ -76,4 +83,48 @@ public class ShtrihFRApplet extends Applet {
         repaint();
         return answer;
     }
+
+    //This focus trics are needed, because in Windows environment, scrolling will be blocked
+    //it's some strange problem, when focus are locked on the applet and never leaves it anymore...or something like this
+    public void focusGained(FocusEvent fe)
+    {
+//        System.out.println("Focus gained...");
+//        System.out.println("Try to BLUR...");
+        JSObject.getWindow(this).eval((new StringBuilder()).append("document.getElementById(\"").append(getParameter("appletId")).append("\").blur()").toString());
+    }
+
+    public void focusLost(FocusEvent fe)
+    {
+//        System.out.println("Focus lost...");
+    }
+    //----------------------------------------------------------
+
+
+
+// This is used instead a tooltip (which is not accessible for applets in HTML)
+// when you drags the mouse over the applet, it's name will be displayed in browser's status bar
+    public void mouseEntered(MouseEvent arg0)
+    {
+//        System.out.println("Mouse entered...");
+        showStatus(appletName);
+    }
+
+    public void mouseExited(MouseEvent arg0)
+    {
+//        System.out.println("Mouse exited...");
+        showStatus("");
+    }
+    public void mouseClicked(MouseEvent mouseevent)
+    {
+    }
+
+    public void mousePressed(MouseEvent mouseevent)
+    {
+    }
+
+    public void mouseReleased(MouseEvent mouseevent)
+    {
+    }
+    //----------------------------------------------------------
+
 }
